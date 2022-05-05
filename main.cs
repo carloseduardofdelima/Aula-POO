c53ae59cf6b699a9be02902f9ecf4aeb0cc691f3
Carlos Eduardo Ferreira de Lima <20211014040010@ifrn.local>
2022-05-05 10:09:14 -0300

    Added main.cs

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int x = int.Parse(Console.ReadLine());
+    int y = int.Parse(Console.ReadLine());
+    int res = ((x * 2) + (y * 3)) / 5;
+    Console.WriteLine(res);
+    
+  }
+}
