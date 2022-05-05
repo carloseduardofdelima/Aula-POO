e3c1939f6e39661de0dce0b4be5e9536aaba7179
Carlos Eduardo Ferreira de Lima <20211014040010@ifrn.local>
2022-05-05 10:21:22 -0300

    Adicionado printNome

---
+++ printNome.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine($"Olá {nome}");
+    }
+}
